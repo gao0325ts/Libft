36b959862eccd2359b96f75b8a9d241475c447da
stakada <stakada@student.42tokyo.jp>
2024-11-08 01:31:06 +0900

    Done ft_strchr, ft_strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 19:34:28 by stakada           #+#    #+#             */
+/*   Updated: 2024/11/07 21:41:15 by stakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strrchr(const char *s, int c)
+{
+	int i;
+
+	i = ft_strlen(s) - 1;
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i--;
+	}
+	return (NULL);
+}
