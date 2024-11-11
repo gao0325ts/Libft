c8af43cb2d30496ce401afdfdaea987f6ccf9680
stakada <160503699+gao0325ts@users.noreply.github.com>
2024-11-11 19:55:43 +0900

    Done bonus part

---
+++ ft_lstclear.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 16:56:15 by stakada           #+#    #+#             */
+/*   Updated: 2024/11/11 19:14:56 by stakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+    t_list  *tmp;
+    
+    while (*lst)
+    {
+        tmp = (*lst)->next;
+        ft_lstdelone(*lst, del);
+        *lst = tmp;
+    }
+}
---
+++ ft_lstmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 19:18:57 by stakada           #+#    #+#             */
+/*   Updated: 2024/11/11 19:43:55 by stakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+    t_list  *start;
+    t_list  *tmp;
+
+    start = NULL;
+    while (lst)
+    {
+        tmp = ft_lstnew(f(lst->content));
+        if (!tmp)
+        {
+            ft_lstclear(&start, del);
+            return (NULL);
+        }
+        ft_lstadd_back(&start, tmp);
+        lst = lst->next;
+    }
+    return (start);
+}
